1e2957003f6197526ccbdc9dd169dc2de7d45f7e
panzerlop <94750871+panzerlop@users.noreply.github.com>
2022-03-23 15:25:49 +0000

    Update macspoofer.sh

--- macspoofer.sh
+++ macspoofer.sh
@@ -2,11 +2,38 @@
 
 sudo apt install macchanger
 
+sudo apt install net-tools
+
+sudo service NetworkManager stop; sleep 5;
+
 # Get list of network interfaces. Excludes loopback and virtual machine interfaces.
 interfaces=$(ls /sys/class/net | grep -v 'lo' | grep -v 'tun0' | grep -v "virbr")
 
+#turn the interfaces off
+for i in ${interfaces}
+do
+
+  sudo ifconfig $i down
+
+done
 # Spoof the MAC address of each.
 for i in ${interfaces}
 do
-  macchanger -e $i >/dev/null # Hide the output so it can't be discovered with systemd logs.
+
+ sudo macchanger -e $i >/dev/null # Hide the output so it can't be discovered with systemd logs.
 done
+
+for i in ${interfaces}
+do
+
+  sudo ifconfig $i up
+
+done
+
+sudo service NetworkManager start;
+
+echo "done"
+
+sleep 5;
+
+
